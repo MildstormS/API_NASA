77db11258e72a35e555cd17fce16d749009fc0ac
MildstormS <110379772+MildstormS@users.noreply.github.com>
2023-01-18 20:27:37 +0500

    Update Planetary.java

--- src/main/java/org/example/Planetary.java
+++ src/main/java/org/example/Planetary.java
@@ -7,8 +7,8 @@ public class Planetary {
     private String date;
     private String explanation;
     private String hdurl;
-    private String media_type;
-    private String service_version;
+    private String mediaType;
+    private String serviceVersion;
     private String title;
     private String url;
 
@@ -17,8 +17,8 @@ public class Planetary {
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url)
     {
@@ -26,8 +26,8 @@ public class Planetary {
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -48,12 +48,12 @@ public class Planetary {
         return hdurl;
     }
 
-    public String getMedia_type() {
-        return media_type;
+    public String getMediaType() {
+        return mediaType;
     }
 
-    public String getService_version() {
-        return service_version;
+    public String getServiceVersion() {
+        return serviceVersion;
     }
 
     public String getTitle() {
@@ -71,8 +71,8 @@ public class Planetary {
                 "\n date =" + date +
                 "\n explanation =" + explanation  +
                 "\n hdurl =" + hdurl  +
-                "\n media_type =" + media_type +
-                "\n service_version =" + service_version +
+                "\n media_type =" + mediaType +
+                "\n service_version =" + serviceVersion +
                 "\n title =" + title +
                 "\n url =" + url;
     }
